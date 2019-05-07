ac36b942cda33589e27f2e783499989435f37534
Alan Pope <alan.pope@canonical.com>
2019-05-07 19:38:04 +0100

    Fix url grabbing
    Fix as @om26er did for android studio - we forgot to update this one too.

--- get_latest.py
+++ get_latest.py
@@ -3,38 +3,25 @@
 import re
 import urllib.request
 
-URL_STUDIO_HOME = 'https://developer.android.com/studio/preview/'
-
+URL_STUDIO_HOME = 'https://developer.android.com/studio/preview/index.html'
 
 def get_latest_studio_url():
     with urllib.request.urlopen(URL_STUDIO_HOME) as response:
         html = response.read().decode()
 
-    matched = re.findall('"((https)?://.*linux.zip)"', html)
+    matched = re.findall('"((https)?://.*linux.tar.gz)"', html)
     # Ensure unique and then convert to a list of easy access.
     links = list(set(matched))
 
     if len(links) == 0:
         raise ValueError('Url matching our query not found.')
-    elif len(links) > 2:
-        raise RuntimeError('More than two urls found, expected two, urls are: {}'.format(
-            ' '.join([link[0] for link in links])))
+    elif len(links) > 1:
+        raise RuntimeError('Multiple urls found, expected only one, urls are: {}'.format(
+            ' '.join(links)))
 
-    url1 = links[0][0]
-    version1 = url1.split('/')[-2]
-
-    url2 = links[1][0]
-    version2 = url2.split('/')[-2]
-
-    version1_broken = list(map(int, version1.split('.')))
-    version2_broken = list(map(int, version2.split('.')))
-
-    for i in range(len(version1_broken)):
-        if version1_broken[i] > version2_broken[i]:
-            return version1, url1
-        elif version1_broken[i] < version2_broken[i]:
-            return version2, url2
-    return version1, url1
+    url = links[0][0]
+    version = url.split('/')[-2]
+    return version, url
 
 
 if __name__ == '__main__':
